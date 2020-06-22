3dcb67735c8a0551d9a2b9c618f10a0721124599
azman-aziz <am911444@gmail.com>
2020-06-22 09:33:48 +0700

    mencoba routes baru

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('about', function(){
+	return "about me?";
 });
+
+Route::view('/', 'welcome');
