e12debe7d79d83e869aeb6fd7d4f3ffb06b5198c
azman-aziz <am911444@gmail.com>
2020-06-24 14:24:34 +0700

    latihan dengan database ke 2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+// memanggil halaman / class
+use \App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function show($slug){
 
-    	$post = \DB::table('posts')->where('slug', $slug)->first();
+    		// first() = di gunakan unutuk mengembalikan value dari db
+    		// firstofFail() = kondisi jika syarat tidak ada maka langsung di arah ke halaman 404
+    	$post = Post::where('slug', $slug)->firstorFail();
     	// DD() =  var_dump()
+
+    	/*
     	if (!$post) {
-    		# code...
-    	}
+    		//di gunakan untuk memanggil halaman 404
+    		abort(404);
+    	}*/
+
+
     	return view('posts.show', compact('post'));
     }
 }
---
+++ app/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// model ini di ambil dari tabel post karena nama nya sama 
+class Post extends Model
+{
+	//validasi tabel untuk model ini
+    // protected $table = 'post' ;
+}
