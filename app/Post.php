143154d1a610d8441b91507ee683c7ceb28b0fac
azman-aziz <am911444@gmail.com>
2020-07-06 14:39:00 +0700

    menambahkan relasi on to many

--- app/Post.php
+++ app/Post.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function scopeLatesFirst(){
-    	return $this->latest()->first();
-    }
+    // public function scopeLatesFirst(){
+    // 	return $this->latest()->first();
+    // }
 
     //fillelable di gunakan untuk menentukan input yang boleh masuk ke database
     // $guarded = semua input di izinkan masuk tanpa terkecuali
     protected $fillable = ['title', 'slug', 'body'];
+
+    public function category(){
+    	return $this->belongsTo(Category::class);
+    }
 }
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,7 +6,10 @@ use App\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
+	//ran(1,3) = hanya 1-3
+	//factory('App\Post', 20)->create(); membuat datapalsu pada tinker
     return [
+    	'category_id' => rand(1, 3),
         'title' => $faker->sentence(),
         'slug' => \Str::slug($faker->sentence()),
         'body' => $faker->paragraph(10),
